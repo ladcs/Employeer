af4614dc4f0f548864baa22e79b9051d1f108b00
hplad <hplad@DESKTOP-I5JQ13V.mshome.net>
2023-04-30 16:28:53 -0300

    feat: comment ToPrint

--- src/main/java/employeer/util/ToPrint.java
+++ src/main/java/employeer/util/ToPrint.java
@@ -14,10 +14,13 @@ import java.util.stream.Collectors;
 import employeer.model.Funcionario;
 import employeer.restAPI.RestApi;
 
+/** class para transformar as informações em strings. */
 public class ToPrint {
 	
-private Map<String, List<String>> personInMonth = new HashMap<String, List<String>>();
+	/** propriedade map para informar as pessoas nascidas nos meses já enviado para o método. */
+    private Map<String, List<String>> personInMonth = new HashMap<String, List<String>>();
 	
+    /** propriedade string com nomes dos meses. */
 	final String[] nameMonth = {
           "janeiro",
           "fevereiro",
@@ -33,26 +36,32 @@ private Map<String, List<String>> personInMonth = new HashMap<String, List<Strin
           "dezembro"  
     };
 	
+	/** propriedade para conversar com API. */
 	final RestApi crud;
 	
+	/** constructor para criar a comunicação com a API. */
 	public ToPrint() {
 		this.crud = new RestApi();
 	}
-		
+	
+	/** formata BigDecimal para uma string com . para milhar e , para decimal. */
     public String remunerationToString(BigDecimal valueToString) {
         DecimalFormat df = new DecimalFormat("#,###.00");
         return df.format(valueToString);
     }
     
+    /** formata localDate para dd/MM/yyyy. */
     public String setBirthdateDayMonthYear(LocalDate birthdateToString) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return birthdateToString.format(formatter);
       }
     
+    /** retorna a propriedade personInMonth. */
     public Map<String, List<String>> getPersonInMonth() {
 		return personInMonth;
 	}
 
+    /** adiciona o mês como chave e as pessoas nascida nesse mês no map personInMonth. */
 	public void getEmployeerInMonth(int month) {
     	List<Funcionario> tableEmployeersData = crud.getAllFunc();
     	
@@ -64,6 +73,7 @@ private Map<String, List<String>> personInMonth = new HashMap<String, List<Strin
         this.personInMonth.put(this.nameMonth[month - 1], employeersName);
     }
 	
+	/** método para buscar o employeer mais velho, retornando o nome e a idade do mesmo. */	
 	public String findOlder() {
         List<Funcionario> tableEmployeersData = crud.getAllFunc();
         
@@ -79,6 +89,7 @@ private Map<String, List<String>> personInMonth = new HashMap<String, List<Strin
 		return retunNameAndAge;
     }
 	
+	/** retorna a os nomes dos employeers ordenado. */
 	public List<String> order(){
         List<Funcionario> tableEmployeersData = crud.getAllFunc();
         
@@ -88,6 +99,7 @@ private Map<String, List<String>> personInMonth = new HashMap<String, List<Strin
                 .collect(Collectors.toList());
 	}
 	
+	/** retorna o valor de todos os salários somados. */
 	public BigDecimal allRemuneration() {
 		List<Funcionario> tableEmployeersData = crud.getAllFunc();
 		
@@ -96,6 +108,7 @@ private Map<String, List<String>> personInMonth = new HashMap<String, List<Strin
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 	}
 	
+	/** retorna o nome e quantos salários ele ganha. */
 	public String numberRemuneration(Funcionario employeer, double min) {
 		BigDecimal multiply = new BigDecimal(1 / min);
 		int numberRemuneration = employeer.getRemuneration().multiply(multiply).intValue();
