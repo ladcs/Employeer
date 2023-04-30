1ec48270571e20af81b7563cb201c23e318d127b
hplad <hplad@DESKTOP-I5JQ13V.mshome.net>
2023-04-30 13:22:26 -0300

    add comment Pessoas

--- src/main/java/employeer/model/Pessoas.java
+++ src/main/java/employeer/model/Pessoas.java
@@ -5,26 +5,34 @@ import java.time.LocalDate;
 import javax.persistence.MappedSuperclass;
 
 /**
- * class Pessoa, base to class Funcionario. 
+ * class Pessoa, base da class Funcionario. 
  */
 @MappedSuperclass
 public class Pessoas {
 	
+	/** criando coluna name, referente ao nome. */
 	private String name;
+	
+	/** criando coluna birthDate, referente a data de nascimento. */
 	private LocalDate birthDate;
 	
+	/** getters e setters das colunas. */
+	/** retorna name. */
 	public String getName() {
 		return name;
 	}
 	
+	/** escreve name. */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/** retorna birthDate. */
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
 	
+	/** escreve birthDate. */
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
