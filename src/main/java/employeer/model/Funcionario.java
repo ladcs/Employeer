ee500095baf3059d557a18c4ad5bfe510acdda0b
hplad <hplad@DESKTOP-I5JQ13V.mshome.net>
2023-04-30 13:19:12 -0300

    add comment in class Funcionario

--- src/main/java/employeer/model/Funcionario.java
+++ src/main/java/employeer/model/Funcionario.java
@@ -7,35 +7,50 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/** Class para criar a entidade do hibernate. Class Funcionario que extende class Pessoas. */
 @Entity
 public class Funcionario extends Pessoas {
 	
+	
+	/** criando coluna Id. */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	/** constructior para poder ter as colunas da class Pessoas. */
 	public Funcionario() {
 		super();
 	}
+	
+	/** criando coluna remuneration, que é referente ao salário. */
 	private BigDecimal remuneration;
+	
+	/** criando coluna job, que é referente a função do funcionário. */
 	private String job;
 
+	/** getters e setters das colunas.*/
+	
+	/** para retornar o Id. */
 	public Long getId() {
 		return id;
 	}
 	
+	/** para retornar o salário. */
 	public BigDecimal getRemuneration() {
 		return remuneration;
 	}
-
+	
+	/** para escrever em remuneration. */
 	public void setRemuneration(BigDecimal remuneration) {
 		this.remuneration = remuneration;
 	}
 
+	/** para retornar a coluna job. */
 	public String getJob() {
 		return job;
 	}
 
+	/** para escrever em job. */
 	public void setJob(String job) {
 		this.job = job;
 	}
